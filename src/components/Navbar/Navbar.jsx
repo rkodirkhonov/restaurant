f62d8f5421b5002409f49c42f31d5e3ccc0a2344
rkodirkhonov <kodirkhonov99@gmail.com>
2023-07-14 01:04:23 +0900

    navbar finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,16 +23,15 @@ return (
     <div className='app__navbar-login'>
       <a href="#login" className='p__opensans'>Log In / Register</a>
       <div />
-      <a href="/" className='p__opensans'>Book Table</a>
+      <a href="#book" className='p__opensans'>Book Table</a>
     </div>
     <div className='app__navbar-smallscreen'>
       <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(!toggleMenu)} />
       
       {toggleMenu && (
-        <div className='app__navbar-smallscrenoverlay flex__center slide-bottom'>
+        <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
         <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
-        <button className='overlay__close' onClick={() => setToggleMenu(false)}>Close</button>
-        <ul className='app__navbar-smallscren-links'>
+        <ul className='app__navbar-smallscreen_links'>
           <li className='p__opensans'><a href="#home">Home</a></li>
           <li className='p__opensans'><a href="#about">About</a></li>
           <li className='p__opensans'><a href="#menu">Menu</a></li>
