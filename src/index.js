07b4c9bedaccea6bbfd8743d742ec7f06197d064
rkodirkhonov <kodirkhonov99@gmail.com>
2023-07-14 00:10:04 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOMClient from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+ const rootElement = document.getElementById('root')
+ const root = ReactDOMClient.createRoot(rootElement);
+ root.render(<App/>);
