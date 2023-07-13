191dc6759c608e2e91d53ed4f310803a3cd7ff05
rkodirkhonov <kodirkhonov99@gmail.com>
2023-07-14 03:39:18 +0900

    aboutus page

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -14,7 +14,6 @@ const Header = () => (
       </p>
       <button type='button' className='custom__button'>Explore Menu</button>
     </div>    
-
     <div className='app__wrapper_img'>
       <img src={images.welcome} alt="header img" />
     </div>
