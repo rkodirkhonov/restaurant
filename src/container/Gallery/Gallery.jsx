95a454c26de8b60c2ef7d53733e2093de6e0d7d1
rkodirkhonov <kodirkhonov99@gmail.com>
2023-07-20 23:50:01 +0900

    gallery page

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -12,9 +12,9 @@ const Gallery = () => {
     const {current} = scrollRef;
 
     if(direction === 'left'){
-      current.scrollLeft -= 300;
+      current.scrollLeft -= 350;
     }else{
-      current.scrollLeft += 300
+      current.scrollLeft += 350;
     }
   }
   return  (
