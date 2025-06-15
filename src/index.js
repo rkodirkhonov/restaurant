05c38da32f4f1bea699f5beea608d9b69d5e9037
Rustamjon <kodirkhonov99@gmail.com>
2025-06-15 23:43:14 +0900

    updated

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-
-import './index.css';
-import App from './App';
- const rootElement = document.getElementById('root')
- const root = ReactDOMClient.createRoot(rootElement);
- root.render(<App/>);
